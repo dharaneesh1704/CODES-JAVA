6fe25b89a9e6ecbe0dac2c91cabc96ca7a0e5418
Dharaneesh S <skdharaneeshsivasamy@gmail.com>
2025-08-08 12:13:30 +0530

    "deleted"

---
+++ TESTS/testa.java
@@ -0,0 +1,8 @@
+public class testa {
+    public static void main(String[] args){
+        double a = 7.657678;
+        System.out.println(a);
+        System.out.println(1+'1');
+        System.out.println(1-'1');
+    }
+}
